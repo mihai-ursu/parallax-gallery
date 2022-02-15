86e59a0b4a9ffbb57ca3ba7600cd7545540801d4
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-02-15 15:44:28 +0200

    Added data file and moved images

---
+++ src/assets/data.js
@@ -0,0 +1,14 @@
+const data = {
+  images: [
+    { name: "Blue water", src: "/images/blue-water.jpg" },
+    { name: "Pier", src: "/images/pier.jpg" },
+    { name: "Rocks in sea", src: "/images/rocks-in-sea.jpg" },
+    { name: "Sea aerial", src: "/images/sea-aerial.jpg" },
+    { name: "Sea umbrellas", src: "/images/sea-umbrellas.jpg" },
+    { name: "Water splash", src: "/images/water-splash.jpg" },
+    { name: "Waves", src: "/images/waves.jpg" },
+    { name: "Whales", src: "/images/whales.jpg" },
+  ],
+};
+
+export default data;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import styles from "../styles/Home.module.scss";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Parallax Gallery</h1>
     </div>
   );
-}
+};
+
+export default Home;
