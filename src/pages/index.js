c6e18a4ed78c53bf45315041ba0c7dc07baaf583
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-02-20 16:59:03 +0200

    Added page title / description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,26 @@
 import styles from "../styles/Home.module.scss";
 import ParallaxGallery from "../components/ParallaxGallery/ParallaxGallery";
 import imagesData from "../assets/data";
+import { Fragment } from "react/cjs/react.production.min";
 
 const Home = () => {
   return (
-    <div className={styles.app}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Parallax Gallery</h1>
-        <ParallaxGallery images={imagesData.images} />
+    <Fragment>
+      <Head>
+        <title>Parallax Gallery</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="A parallax galler for React / NextJS."
+        />
+      </Head>
+      <div className={styles.app}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Parallax Gallery</h1>
+          <ParallaxGallery images={imagesData.images} />
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
